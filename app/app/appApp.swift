cf3318da210d6376314505be44703933d20a77ce
MevlutArslan <mevlut12098@gmail.com>
2024-01-27 10:26:09 -0600

    app app subrepo

---
+++ app/app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Mevlut Arslan on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

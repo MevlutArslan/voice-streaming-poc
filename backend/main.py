ff6b57bad442877dff7d3d38d3fe98e3602d669d
MevlutArslan <mevlut12098@gmail.com>
2024-02-04 13:55:24 -0600

    clean up

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,11 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from connection_manager import ConnectionManager, get_eth0_ip
-from typing import List
-import time
 import asyncio
-from io import BytesIO
+from audio import is_audio_silent, save_audio_as_file, get_transcription
+import numpy as np
+
 app = FastAPI()
 manager = ConnectionManager()
-
-import pyaudio
-from audio import is_audio_silent, save_audio_as_file, get_transcription
-
-audio_manager = pyaudio.PyAudio()
-
-# DISCLAIMER: Ideally you would send these from the client right 
-#             before you open the stream instead of hardcoding it.
-# Hardcoded audio parameters
-rate = 48000
-channels = 1
-format = pyaudio.paFloat32
-
-import numpy as np
-# Create PyAudio stream with hardcoded parameters
-# stream = audio_manager.open(rate=rate, channels=channels, format=format, output=True)
 
 @app.websocket("/communicate")
 async def websocket_endpoint(websocket: WebSocket):
@@ -45,21 +29,6 @@ async def websocket_endpoint(websocket: WebSocket):
                     print(text)
                     accumulated_audio = np.array([], dtype=np.float32)
                 continue
-
-            # is_silent = is_audio_silent(data, threshold=1.0)
-
-            # if is_silent:
-            #     # Check if silence exceeds 2 seconds
-            #     current_time = time.time()
-            #     if current_time - last_non_silent_time > 2:
-            #         audio_file_path = save_audio_as_file(accumulated_audio)
-            #         transcription = get_transcription(audio_file_path)
-            #         print(transcription)
-            #         accumulated_audio.clear()
-            # else:
-            #     last_non_silent_time = time.time()
-            #     accumulated_audio.append(data)
-
     except WebSocketDisconnect:
         manager.disconnect(websocket)
     finally:

4515cfc2811a9d07a23dbb31132b26f78acee22d
MevlutArslan <mevlut12098@gmail.com>
2024-02-09 12:03:06 -0600

    implement audio accumulation

--- backend/main.py
+++ backend/main.py
@@ -3,38 +3,56 @@ from connection_manager import ConnectionManager, get_eth0_ip
 import asyncio
 from audio import is_audio_silent, save_audio_as_file, get_transcription
 import numpy as np
-from llm import message_llm
+import time
+import io
+
 app = FastAPI()
 manager = ConnectionManager()
 
 @app.websocket("/communicate")
 async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
-    accumulated_audio = np.array([], dtype=np.float32)
+    accumulated_bytes = io.BytesIO()
+    audio_accumulation_threshold = 5.0
+    audio_silence_threshold = 3.0
+    
+    accumulated_audio_len = 0.0
+    audio_sent_by_client_len = 0.1 # this is a fact that I checked on the client
     try:
         while True:
+            process_data = False
             try:
-                data = await asyncio.wait_for(websocket.receive_bytes(), timeout=2)
-                # turn bytes object into a np array of type float32
-                audio_chunk = np.frombuffer(data, dtype=np.float32)
-                # accumulate the arrays
-                accumulated_audio = np.concatenate((accumulated_audio, audio_chunk))
+                # if we haven't received anything for audio_silence_threshold duration then we should
+                # process the data in the buffer
+                data = await asyncio.wait_for(websocket.receive_bytes(), audio_silence_threshold)
+                
+                accumulated_bytes.write(data)
+                accumulated_audio_len += audio_sent_by_client_len
+                
+                if accumulated_audio_len >= audio_accumulation_threshold:
+                    print("Received audio for the threshold amount, triggering execution")
+                    process_data = True
             except asyncio.TimeoutError:
-                if len(accumulated_audio) > 0:
-                    audio_file_path = save_audio_as_file(accumulated_audio)
-                    text = get_transcription(audio_file_path)
-
-                    for audio_chunk in message_llm(text):
-                        await websocket.send_bytes(audio_chunk)                    
-                    
-                    accumulated_audio = np.array([], dtype=np.float32)
-                continue
+                # make sure the accumulated bytes arent empty:
+                if accumulated_bytes.tell() > 0:
+                    print("Did not receive audio for the threshold amount, triggering execution")
+                    process_data = True
+                
+            if process_data:
+                print("Processing Data...")
+                accumulated_bytes.seek(0)
+                
+                # clear out the buffer
+                accumulated_bytes.seek(0)
+                accumulated_bytes.truncate(0)
+                # -------------------------------
+                accumulated_audio_len = 0.0
     except WebSocketDisconnect:
         manager.disconnect(websocket)
     finally:
         # Clean up resources, stop the stream, and close it
         manager.disconnect(websocket)
-
+        
 if __name__ == "__main__":
     ip_address = get_eth0_ip()
     if ip_address:
